ce7069fdf3d9165be41267c02cf197ffe495874c
szarp <lizusfull@gmail.com>
2019-10-16 23:41:38 +0200

    try login with passport.js

--- backend/rekrutacja.js
+++ backend/rekrutacja.js
@@ -1,4 +1,184 @@
+var express = require("express");
+var passport = require("passport");
+var Strategy = require('passport-local').Strategy;
 
+
+const app = express()
+const port = 3000
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.post('/login', passport.authenticate('local', { successRedirect: '/',failureRedirect: '/login' }));
+
+
+// my not so secret secret
+var secret = 'eeeek',
+ 
+// the single user record that is hard
+// coded in for the sake of this simple demo
+user = {
+    username: 'foo',
+    id: 0,
+    password: '123'
+};
+// using ejs for rendering
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+ 
+// using body parser to parse the body of incoming post requests
+app.use(require('body-parser').urlencoded({
+    extended: true // must give a value for extended
+}));
+ 
+// using express-session for session cookies
+app.use(
+ 
+    require('express-session')(
+ 
+        {
+            name: 'site_cookie',
+            secret: secret,
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+ 
+                // make session cookies only last 15 seconds
+                // for the sake of this demo
+                maxAge: 15000
+ 
+            }
+        }
+ 
+    )
+ 
+);
+ 
+// using the local strategy with passport
+passport.use(
+ 
+    // calling the constructor given by passport-local
+    new Strategy(
+ 
+        // options for passport local
+        {
+ 
+            // using custom field names
+            usernameField: 'user',
+            passwordField: 'pass'
+ 
+        },
+ 
+        // login method
+        function (username, password, cb) {
+ 
+            if (username === user.username && password.toString() === user.password) {
+ 
+                return cb(null, user);
+ 
+            }
+ 
+            // null and false for all other cases
+            return cb(null, false);
+ 
+        }
+ 
+    )
+ 
+);
+ 
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
+});
+ 
+passport.deserializeUser(function (id, cb) {
+ 
+    cb(null, user);
+ 
+});
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+app.get('/', function (req, res) {
+ 
+    res.render('index', {
+        layout: 'home',
+        user: req.user
+    });
+ 
+});
+ 
+app.get('/login',
+ 
+    function (req, res) {
+    res.render('index', {
+        layout: 'login',
+        user: req.user
+    });
+});
+/*
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+app.get('/server', ensureAuthenticated, routes.server.get);
+app.get('/login', routes.login.get);
+*/
+app.post('/dystans',function (req, res, next) {
+    if (req.isAuthenticated()) {
+        console.log(req.body)
+        console.log("logged");    
+        
+        //res.sendStatus(200);
+        res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+    }
+    else
+    res.redirect('/login');
+});
+app.get('/dystans',function (req, res) {
+    res.render('index', {
+        layout: 'dystans',
+        user: req.user
+    });
+});
+    
+ 
+    // end up at / if login works
+    
+app.post('/login',
+    passport.authenticate('local', {
+        // redirect back to /login
+        // if login fails
+        failureRedirect: '/login'
+    }),
+ 
+    // end up at / if login works
+    function (req, res) {
+        res.redirect('/');
+    }
+);
+ 
+app.get('/logout',
+    function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+ 
+app.listen(port, function () {
+ 
+    console.log('passport-local demo up on port: ' + port);
+ 
+});
+app.get('/login',
+ 
+    function (req, res) {
+    res.render('index', {
+        layout: 'login',
+        user: req.user
+    });
+});
 ///****************** */
 var fs = require('fs');
 //
